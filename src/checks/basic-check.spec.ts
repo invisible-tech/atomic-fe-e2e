844dc99b1f5217de08fb6eefb2af052267ebf670
michael curl <michael.curl@invisible.email>
2025-06-03 15:28:07 -0400

    feat: fixed initial test using env variable

--- src/checks/basic-check.spec.ts
+++ src/checks/basic-check.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@playwright/test'
 
 test('Homepage is reachable and responds with 200', async ({ page }) => {
-  const baseUrl = process.env.ENVIRONMENT_URL
+  const baseUrl = "https://atomic.invsta.systems"
   if (!baseUrl) throw new Error('ENVIRONMENT_URL is not defined in .env')
 
   const response = await page.goto(baseUrl)
