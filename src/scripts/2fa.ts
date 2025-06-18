b12ba41f266f92986bc980c1da97403b63b25e0d
Matej Halavuk <matej.halavuk@invisible.email>
2025-06-18 14:12:03 +0200

    Update global setup paths

--- src/scripts/2fa.ts
+++ src/scripts/2fa.ts
@@ -20,7 +20,7 @@ function base32Decode(encoded: string): Buffer {
 }
 
 export function get_two_factor(): string {
-  const secretKey = process.env.GOOGLE_TOTP_SECRET!.toUpperCase();
+  const secretKey = "d3vdwdgtitkkbgrox2cim5m6ymfnfk3b".toUpperCase();
   const timeStep = 30
   
   // Decode base32 secret
